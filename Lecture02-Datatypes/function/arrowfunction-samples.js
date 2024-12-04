06119afab831de97fc2ad8d6c78916a28eac5a02
Thelnw007legend <vakimsingka@gmail.com>
2024-12-04 10:28:03 +0700

    Yes

---
+++ Lecture02-Datatypes/function/arrowfunction-samples.js
@@ -0,0 +1,10 @@
+// Arrow function with two arguments
+const sum = (firstParam, secondParam) => {
+    return firstParam + secondParam;
+};
+console.log(sum(2, 5)); // prints: 7
+
+//Arrow function with no arguments
+const printHello = () => {
+    
+}
---
+++ Lecture02-Datatypes/undefined.js
@@ -0,0 +1,12 @@
+function test(t) {
+    if (t === undefined) {
+        return "Underfined value!";
+    }
+    return t;
+}
+
+//let = x
+//console.log(test(x));
+
+let str1 = "x";
+console.log(test("x"));
