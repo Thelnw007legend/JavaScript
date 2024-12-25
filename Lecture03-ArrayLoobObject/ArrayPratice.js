df008063015f19c63228e225e65a39a57ef7cd26
Thelnw007legend <vakimsingka@gmail.com>
2024-12-25 10:47:43 +0700

    Yes

---
+++ Lecture03-ArrayLoobObject/ArrayPratice.js
@@ -0,0 +1,5 @@
+let furniture = ['Table', 'Chairs', 'Couch'];
+
+for (let char of furniture) {
+    console.log( char );
+}
--- Lecture03-ArrayLoobObject/cloneObj.js
+++ Lecture03-ArrayLoobObject/cloneObj.js
@@ -9,9 +9,8 @@ const obj1 = {
 }
 
 //cosnt obj2 = obj1
-//const obj2 = Object.assign({}, obj1);
-
-const obj2 = {... obj1};
+const obj2 = Object.assign({}, obj1);
+//const obj2 = {... obj1};
 obj2.weight = 75;
 console.log(obj1);
 console.log(obj2);
